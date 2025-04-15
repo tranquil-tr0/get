ed8396eddd0a17e401716d49709132e7f937bb4f
tranquil-tr0 <tranquiltr0@proton.me>
2025-04-14 23:17:56 -0700

    style: improve formatting in package list output

--- pkg/manager/list.go
+++ pkg/manager/list.go
@@ -15,12 +15,13 @@ func (pm *PackageManager) PrintInstalledPackages() error {
 		return nil
 	} else {
 		output.PrintTitle("Installed packages:")
+		output.PrintTitle("----------------------------------")
 	}
 
 	for _, pkg := range metadata.Packages {
 		output.PrintNormal("%s/%s (Version: %s, Installed: %s)", output.Bold(pkg.Owner), output.Bold(pkg.Repo), pkg.Version, pkg.InstalledAt)
 		if pkg.AptName != "" {
-			output.PrintGreen("    APT Package Name: %s", pkg.AptName)
+			output.PrintGreen("  - APT Package Name: %s", pkg.AptName)
 		}
 		output.PrintNormal("")
 	}
